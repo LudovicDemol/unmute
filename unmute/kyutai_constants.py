86bdc0f53f6171564706e8a0328759db6cf2f724
LudovicDemol <demol.ludovic@yahoo.fr>
2026-03-30 13:52:21 +0000

    embedding wav

--- unmute/kyutai_constants.py
+++ unmute/kyutai_constants.py
@@ -8,7 +8,7 @@ HEADERS = {"kyutai-api-key": "public_token"}
 # The defaults are already ws://, but make the env vars support http:// and https://
 STT_SERVER = http_to_ws(os.environ.get("KYUTAI_STT_URL", "ws://localhost:8090"))
 TTS_SERVER = http_to_ws(os.environ.get("KYUTAI_TTS_URL", "ws://localhost:8089"))
-LLM_SERVER = "https://api.groq.com/openai/v1"
+LLM_SERVER = "https://api.groq.com/openai"
 KYUTAI_LLM_MODEL = "llama-3.3-70b-versatile"
 KYUTAI_LLM_API_KEY = "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"
 VOICE_CLONING_SERVER = os.environ.get(
