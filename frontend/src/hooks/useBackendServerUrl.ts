22cf597fd654e10fa18456b3aada658c1653e8e8
Dolu13 <demol.ludovic@yahoo.fr>
2026-04-01 10:56:57 +0200

    suppression frontend

--- frontend/src/hooks/useBackendServerUrl.ts
+++
@@ -1,3 +0,0 @@
-export const useBackendServerUrl = () => {
-  return process.env.NEXT_PUBLIC_URL_API_UNMUTE ?? null
-}
--- frontend/src/utils/recordingConsent.ts
+++
@@ -1,7 +0,0 @@
-// utils/recordingConsent.ts
-// Utilitaire pour gérer le consentement d'enregistrement
-import { RECORDING_CONSENT_STORAGE_KEY } from "../components/ConsentModal";
-
-export function getRecordingConsent(): boolean {
-  return localStorage.getItem(RECORDING_CONSENT_STORAGE_KEY) === "true";
-}
